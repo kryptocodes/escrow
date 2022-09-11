ba6a09c789175eb24516b505892ca8ab4df729d7
kryptocodes <srivatsantb@gmail.com>
2022-09-11 21:31:53 +0530

    initial with escrow contract [] - add 721 minter [] - verifier
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

---
+++ contracts/escrow.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <=0.9.0;
+
+contract Escrow {
+    address agent;
+    mapping(address => uint256) public deposits;
+
+    modifier onlyAgent(){
+        require(msg.sender == agent,"Only agent");
+        _;
+    }
+    constructor () {
+        agent = msg.sender;
+    }
+
+    function deposit (address payee) public onlyAgent payable {
+        uint256 amount = msg.value;
+        deposits[payee] = deposits[payee] + amount;
+    }
+
+    function withdraw(address payable payee)  public onlyAgent {
+        uint256 payment = deposits[payee];
+        deposits[payee] = 0;
+        payee.transfer(payment);
+    }
+}
